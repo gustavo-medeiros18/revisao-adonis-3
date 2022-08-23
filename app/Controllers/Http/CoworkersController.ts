c13aacad799cd3204b213694b3812b3a7a3edd59
Gustavo Medeiros <gugamedeiros517@gmail.com>
2022-08-23 16:27:40 -0300

    adiciona método Coworker.store() no model

---
+++ app/Controllers/Http/CoworkersController.ts
@@ -0,0 +1,12 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Coworker from 'App/Models/Coworker';
+
+export default class CoworkersController {
+  public async store({ request, response }: HttpContextContract) {
+    const coworkerData = request.body();
+    const results = await Coworker.create(coworkerData);
+
+    response.status(201);
+    return results;
+  }
+}
